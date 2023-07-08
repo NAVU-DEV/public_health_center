37beacb7c8aeb300577678a98621b2ad1c45af6a
“n0tavaliduser” <stevanoputra38@gmail.com>
2023-07-08 23:03:50 +0700

    Site Controller: Page Flow

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Berita;
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function index()
+    {
+        $semua_berita = Berita::orderBy('created_at', 'ASC')->paginate(5);
+        
+        return view('public.home', [
+            'semua_berita' => $semua_berita
+        ]);
+    }
+}
