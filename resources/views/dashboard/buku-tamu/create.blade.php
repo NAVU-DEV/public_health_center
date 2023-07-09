eaf485ace53543dfbf660d5115c5154de2953d7d
“n0tavaliduser” <stevanoputra38@gmail.com>
2023-07-09 16:09:27 +0700

    Buku Tamu Create UI

--- resources/views/dashboard/buku-tamu/create.blade.php
+++ resources/views/dashboard/buku-tamu/create.blade.php
@@ -6,11 +6,11 @@
         <div class="card">
             <div class="card-body">
                 <h3>Tambah Buku Tamu</h3>
-                <form method="post" action="{{ url('/buku-tamu') }}" enctype="multipart/form-data">
+                <form method="post" action="{{ route('buku-tamu.store') }}" enctype="multipart/form-data">
                     @csrf
                     @method('POST')
                     <div class="form-floating mb-4">
-                        <input id="date" type="date" name="date" value="{{ now() }}" class="form-control" placeholder="Date Input" readonly>
+                        <input id="date" type="date" name="gb_date" value="{{ now() }}" class="form-control" placeholder="Date Input" readonly>
                         @push('scripts')
                             <script>
                                 document.getElementById('date').value = new Date().toISOString().substr(0, 10);
@@ -19,15 +19,15 @@
                         <label for="date">Date Input</label>
                     </div>
                     <div class="form-floating mb-4">
-                        <input id="name" type="text" name="name" value="{{ old('name') }}" class="form-control" placeholder="Name Input">
+                        <input id="name" type="text" name="name" value="{{ old('name') }}" class="form-control {{ !$errors->has('name')?:'is-invalid' }}" placeholder="Name Input">
                         <label for="name">Name Input</label>
                     </div>
                     <div class="form-floating mb-4">
-                        <input id="age" type="number" name="age" value="{{ old('age') }}" class="form-control" placeholder="Age Input">
+                        <input id="age" type="number" name="age" value="{{ old('age') }}" class="form-control {{ !$errors->has('age')?:'is-invalid' }}" placeholder="Age Input">
                         <label for="age">Age Input</label>
                     </div>
                     <div class="form-floating mb-4">
-                        <textarea id="description" class="form-control" name="description" placeholder="Description Input" style="height: 150px">{{ old('description') }}</textarea>
+                        <textarea id="description" class="form-control {{ !$errors->has('description')?:'is-invalid' }}" name="description" placeholder="Description Input" style="height: 150px">{{ old('description') }}</textarea>
                         <label for="description">Description</label>
                     </div>
                     <div class="d-flex justify-content-end">
